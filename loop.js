0078c69a8e6da2a83862f0b3808815fc40c77bdc
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-14 13:59:16 -0400

    more addition

--- loop.js
+++ loop.js
@@ -19,6 +19,28 @@ const array = [2, 3, 3, 5, 3, 2, 4]
 // }
 
 //print backwards
-for (let i = array.length - 1; i > -1; i--) {
-  console.log(array[i])
+// for (let i = array.length - 1; i > -1; i--) {
+//   console.log(array[i])
+// }
+
+// sum = 0
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i])
+//   sum = sum + array[i]
+// }
+//  console.log(sum)
+
+
+//infinite loop (crtl + c to stop)
+// for (let i = 1; i > 0; i++) {
+//   console.log(i)
+// }
+
+
+for(let i = 10; i >= 0; i--){
+    if (i === 0){
+        console.log('BLAST OFF')
+        return;
+    }
+    console.log(i)
 }
