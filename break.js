e261e7afbf5c8abf3754a92dc42e19e85ef35313
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-14 15:06:40 -0400

    break & continue

---
+++ break.js
@@ -0,0 +1,7 @@
+//break terminate the most inner loop
+for (let i = 0; i < 15; i++){
+    if(i === 12) {
+        break
+    }
+    console.log(i)
+}
---
+++ continue.js
@@ -0,0 +1,8 @@
+//Restarts the most inner loop
+for (let i = 0; i < 15; i++){
+    if(i === 12) {
+        console.log('Hi i am number 12')
+        continue
+    }
+    console.log(i)
+}
