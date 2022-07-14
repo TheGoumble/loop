6c1a2981e6180389f1dfd3a9d6bb9d518440554c
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-07-14 13:10:55 -0400

    intro to loops

---
+++ for-loop.js
@@ -0,0 +1,13 @@
+// for ([initionalization]; [condition]; [iteration]){
+//     statement
+// }
+
+for(let i = 0; i < 10; i++){
+    console.log('Iteration Number', i)
+}
+
+
+//initialiaztion = Typically used to initalize a counter variable
+//conditon - Runs untill condition is false
+//iteration - Generally used to update or incraement the counter variable
+//statement - code that is excuted while the condition is true
